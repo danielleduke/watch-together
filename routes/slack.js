c65306bbad290cd22f164cdbcba21a98089e6b65
danielleduke <63257732+danielleduke@users.noreply.github.com>
2020-06-24 16:20:03 -0400

    Updates=

--- routes/slack.js
+++ routes/slack.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const slackTools = require ('../tools/slack-tools');
+const { WebClient } = require('@slack/web-api');
+const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,5 +15,17 @@ router.get('/word/:myword', function(req, res, next) {
   //send that word to slack
   res.render('index', { title: `Word=${req.params.myword}` });
 });
+
+router.get('/channels', async function(req, res, next){
+  console.log("searching for channels");
+  const result = await web.conversations.list({});
+  res.json(result);
+})
+
+router.get('/emojis', async function(req, res, next){
+  console.log("searching for custom emojis");
+  const result = await web.emoji.list({});
+  res.json(result);
+})
 
 module.exports = router;
